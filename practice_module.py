7f8e86f3a9da49b9b1fadc21ffe9b1e91b6b66f0
Austin Aldrich <a-aldrich@hotmail.com>
2023-04-20 00:03:52 -0700

    first commit

---
+++ practice_module.py
@@ -0,0 +1,16 @@
+def area_calculator(l, w):
+    """
+        area_calculator(length, width)
+
+        Returns the area of a rectangle by multiplying a given length and width.
+    """
+    return l*w
+
+def circle_circum(r):
+    """
+    circle_circum(radius)
+
+    Returns the circumference of a circle given a radius.
+    """
+    from math import pi
+    return 2 * pi * r
